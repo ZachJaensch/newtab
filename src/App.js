079b1ac5a5e4762d28caede04eb54ae83fbf9d9d
Zach Jaensch <16172850+ZachJaensch@users.noreply.github.com>
2020-03-18 21:30:10 +1100

    add a background image

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@ import CurrentWeather from './components/CurrentWeather'
 
 const StyledApp = styled.div`
   font-family: sans-serif;
+  color: lightgray;
+`
+
+const Background = styled.div`
+  background-image: url(background.jpg);
+  background-size: 100vw 100%; 
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
 `
 
 function App() {
   return (
     <StyledApp>
+      <Background />
       <WeatherProvider>
-        <h1>
-          This is your new tab
-        </h1>
         <CurrentWeather/>
       </WeatherProvider>
     </StyledApp>
